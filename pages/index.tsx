61b36c5274f7ce2ea4aaaed381609e3dc7ced189
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2023-04-08 17:39:19 +0200

    SolidJS rewrite Fixes #2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import { Accessor, createSignal, Setter } from 'solid-js';
 import { render } from 'solid-js/web';
 import { ContextMenu, Option } from "../src"
+
+let yo: Accessor<number>[] = []
+let yo2: Setter<number>[] = []
+window.yo = yo;
+window.yo2 = yo2;
+
+for (let i = 0; i < 1000000; i++) {
+  let test = createSignal(1)
+  yo[i] = test[0];
+  yo2[i] = test[1];
+}
+
 
 function HelloWorld() {
   return (
